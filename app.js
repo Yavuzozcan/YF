a4dbf347beaa421544d196f02de30acc202fb143
Yavuzozcan <yvz.ozcan567@gmail.com>
2026-08-03 20:31:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -199,6 +199,7 @@ const transactionModalBackdrop =
 
   transactionModal?.classList.remove("hidden");
   document.body.style.overflow = "hidden";
+      
 });
 
 closeTransactionFormButton?.addEventListener("click", () => {
@@ -223,6 +224,36 @@ transactionModalBackdrop?.addEventListener("click", () => {
     transactionCard.value = "";
   }
 });
+    transactionModal?.classList.remove("hidden");
+document.body.style.overflow = "hidden";
+});
+
+const quickAddIncomeButton =
+  document.getElementById("quickAddIncomeButton");
+
+const quickAddExpenseButton =
+  document.getElementById("quickAddExpenseButton");
+
+const quickReportsButton =
+  document.getElementById("quickReportsButton");
+
+quickAddIncomeButton?.addEventListener("click", () => {
+  showPage("transactionsPage");
+  transactionType.value = "income";
+  openTransactionFormButton?.click();
+});
+
+quickAddExpenseButton?.addEventListener("click", () => {
+  showPage("transactionsPage");
+  transactionType.value = "expense";
+  openTransactionFormButton?.click();
+});
+
+quickReportsButton?.addEventListener("click", () => {
+  showPage("analysisPage");
+});
+
+closeTransactionFormButton?.addEventListener(
   }
 
   function openNewCardForm() {
